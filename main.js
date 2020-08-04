97421a2ad81bbb0a6feaf1a19eddb0575167678a
Francis Eclevia <francis.eclevia@codeimmersives.com>
2020-08-04 02:45:05 -0400

    hw

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-1
+const paragraph = document.querySelector('p')
+paragraph.style.color = 'lightblue';
+paragraph.innerHTML = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.';
+
+const heading = document.querySelector('head')
+heading.style.fontSize = '5rem';
+
+const item13 = document.querySelector('#item-13')
+item13.style.opacity = "0.5";
+
+const item3 = document.querySelector('#item-3')
+item3.innerHTML = 'I say, "Hi!';
+
+const changeImage = document.querySelector('img')
+changeImage.src = 'http://www.tioxic.com/wp-content/uploads/trex_4.jpg';
+
+
+
+
+
+// const item4 = document.querySelector('#item-4')
+// item4.style.fontSize = '8rem';
+// item4.style.color = 'orange';
